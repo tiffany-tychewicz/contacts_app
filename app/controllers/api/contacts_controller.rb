3784210dcfeb402ae1f237d1b14c83fc6e604519
Siva <carah.tyche@gmail.com>
2019-06-27 20:40:14 -0500

    Initial commit

---
+++ app/controllers/api/contacts_controller.rb
@@ -0,0 +1,40 @@
+class Api::ContactsController < ApplicationController
+
+  def index
+    @contact = Contact.all
+    render "index.json.jb"
+  end
+
+  def show
+    @contact = Contact.find_by(id: params[:id])
+    render "show.json.jb"
+  end
+
+  def create
+    @contact = Contact.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      phone_number: params[:phone_number],
+      email: params[:email]
+      )
+    @contact.save
+    render "show.json.jb"
+  end
+
+  def update
+    @contact = Contact.find_by(id: params[:id])
+    @contact.first_name = params[:first_name] || contact.first_name
+    @contact.last_name = params[:last_name] || contact.last_name
+    @contact.email = params[:email] || contact.email
+    @contact.phone_number = params[:phone_number] || contact.phone_number
+    contact.save
+    render "show.json.jb"
+  end
+
+  def destroy
+    @contact = Contact.find_by(id: params[:id])
+    contact.destroy
+    render json: {message: "Please press F."}
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  namespace :api do
+    get "/contacts" => "contacts#index"
+    get "/contacts/:id" => "contacts#show"
+    post "/contacts" => "contacts#create"
+    patch "/contacts/:id" => "contacts#update"
+  end
+end
